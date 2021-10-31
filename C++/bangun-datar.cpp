14a4b126ed078ac09b85fa1b246d9946544f1fa4
Nas Devs <89463001+nasdevs@users.noreply.github.com>
2021-11-01 07:38:44 +0800

    Update bangun-datar.cpp

--- C++/bangun-datar.cpp
+++ C++/bangun-datar.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <math.h>
-#include <conio.h>
 #include <unistd.h>
-#include <stdio.h>
+#define PHI 3.14
 using namespace std;
 
 void menuBangunDatar() {
@@ -83,15 +82,15 @@ void menuRumusLingkaran() {
     cout << "=========================================" << endl;
 }
 
-void luas(double hasil) {
+void luas(float hasil) {
     cout << "Luas yang didapatkan : " << hasil << endl;
 }
 
-void keliling(double hasil) {
+void keliling(float hasil) {
     cout << "Keliing yang didapatkan : " << hasil << endl;
 }
 
-void diagonal(double hasil) {
+void diagonal(float hasil) {
     cout << "Diagonal yang didapatkan : " << hasil << endl;
 }
 
@@ -106,230 +105,231 @@ void salahInput() {
 }
 
 int main() {
-    int bangunDatar;
+    string bangunDatar;
     char ulang;
 
-    menuBangunDatar();
-    cout << "Pilih [1/2/3/4/5/6/7] : ";
-    cin >> bangunDatar;
+    do {
+        menuBangunDatar();
+        cout << "Pilih : "; cin >> bangunDatar;
 
-    // Persegi
-    if(bangunDatar == 1) {
-        menuRumusPersegi();
-        int rumusPersegi;
+        // Persegi
+        if (bangunDatar == "1" || bangunDatar == "persegi") {
+            menuRumusPersegi();
+            string rumusPersegi;
+            cout << "Pilih : "; cin >> rumusPersegi;
 
-        cout << "Pilih [1/2/3]: ";
-        cin >> rumusPersegi;
+            // Luas
+            if (rumusPersegi == "1" || rumusPersegi == "luas") {
+                int sisi;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung luas persegi" << endl;
+                cout << "Masukkan sisi : "; cin >> sisi;
 
-        if(rumusPersegi == 1) {
-            double sisi, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung luas persegi" << endl;
-            cout << "Masukkan sisi : ";
-            cin >> sisi;
-
-            hasil = sisi * sisi;
-            luas(hasil);
-        }
-
-        else if(rumusPersegi == 2) {
-            double sisi, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung keliling persegi" << endl;
-            cout << "Masukkan sisi : ";
-            cin >> sisi;
-
-            hasil = 4 * sisi;
-            keliling(hasil);
-        }
-
-        else if(rumusPersegi == 3) {
-            double sisi, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung diagonal persegi" << endl;
-            cout << "Masukkan sisi : ";
-            cin >> sisi;
-
-            hasil = sisi * sqrt(2);
-            diagonal(hasil);
-        }
-
-        else {
-            salahInput();
-        }
-    }
-
-    // Persegi Panjang
-    else if(bangunDatar == 2) {
-        menuRumusPersegiPanjang();
-        int rumusPersegiPanjang;
-
-        cout << "Pilih [1/2/3] : ";
-        cin >> rumusPersegiPanjang;
-
-        if(rumusPersegiPanjang == 1) {
-            double panjang, lebar, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung luas persegi panjang" << endl;
-            cout << "Masukkan panjang : ";
-            cin >> panjang;
-            cout << "Masukkan lebar : ";
-            cin >> lebar;
-
-            hasil = panjang * lebar;
-            luas(hasil);
-        }
-
-        else if(rumusPersegiPanjang == 2) {
-            double panjang, lebar, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung keliling persegi panjang" << endl;
-            cout << "Masukkan panjang : ";
-            cin >> panjang;
-            cout << "Masukkan lebar : ";
-            cin >> lebar;
+                hasil = sisi * sisi;
+                luas(hasil);
+            }
             
-            hasil = 2 * (panjang + lebar);
-            keliling(hasil);
+            // Keliling
+            else if (rumusPersegi == "2" || rumusPersegi == "keliling") {
+                int sisi;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung keliling persegi" << endl;
+                cout << "Masukkan sisi : "; cin >> sisi;
+
+                hasil = 4 * sisi;
+                keliling(hasil);
+            }
+
+            // Diagonal
+            else if (rumusPersegi == "3" || rumusPersegi == "diagonal") {
+                int sisi;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung diagonal persegi" << endl;
+                cout << "Masukkan sisi : "; cin >> sisi;
+
+                hasil = sisi * sqrt(2);
+                diagonal(hasil);
+            }
+
+            else {
+                salahInput();
+            }
         }
 
-        else if(rumusPersegiPanjang == 3) {
-            double panjang, lebar, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung diagonal persegi panjang" << endl;
-            cout << "Masukkan panjang : ";
-            cin >> panjang;
-            cout << "Masukkan lebar";
-            cin >> lebar;
+        // Persegi Panjang
+        else if (bangunDatar == "2" || bangunDatar == "persegipanjang") {
+            menuRumusPersegiPanjang();
+            string rumusPersegiPanjang;
+            cout << "Pilih : "; cin >> rumusPersegiPanjang;
+
+            // Luas
+            if (rumusPersegiPanjang == "1" || rumusPersegiPanjang == "luas") {
+                int panjang, lebar;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung luas persegi panjang" << endl;
+                cout << "Masukkan panjang : "; cin >> panjang;
+                cout << "Masukkan lebar   : "; cin >> lebar;
+
+                hasil = panjang * lebar;
+                luas(hasil);
+            }
             
-            hasil = sqrt(pow(panjang, 2) + pow(lebar, 2));
-            diagonal(hasil);
+            // Keliling
+            else if (rumusPersegiPanjang == "2" || rumusPersegiPanjang == "keliling") {
+                int panjang, lebar;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung keliling persegi panjang" << endl;
+                cout << "Masukkan panjang : "; cin >> panjang;
+                cout << "Masukkan lebar   : "; cin >> lebar;
+                
+                hasil = 2 * (panjang + lebar);
+                keliling(hasil);
+            }
+
+            // Diagonal
+            else if (rumusPersegiPanjang == "3" || rumusPersegiPanjang == "diagonal") {
+                int panjang, lebar;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung diagonal persegi panjang" << endl;
+                cout << "Masukkan panjang : "; cin >> panjang;
+                cout << "Masukkan lebar   : "; cin >> lebar;
+                
+                hasil = sqrt(pow(panjang, 2) + pow(lebar, 2));
+                diagonal(hasil);
+            }
+
+            else {
+                salahInput();
+            }
         }
 
+        // Segitiga
+        else if (bangunDatar == "3" || bangunDatar == "segitiga") {
+            // Segitiga, mau dibuatin cabang segitiga lagi seperti
+            // segitiga sama sisi, siku siku, sembarang, sama kaki dll
+            cout << "Coming soon" << endl;
+        }
+
+        // Jajar Genjang
+        else if (bangunDatar == "4" || bangunDatar == "jajargenjang") {
+            menuRumusJajarGenjang();
+            string rumusJajarGenjang;
+            cout << "Pilih : "; cin >> rumusJajarGenjang;
+
+            // Luas
+            if (rumusJajarGenjang == "1" || rumusJajarGenjang == "luas") {
+                int alas, tinggi;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung luas Jajar genjang" << endl;
+                cout << "Masukkan alas   : "; cin >> alas;
+                cout << "Masukkan tinggi : "; cin >> tinggi;
+
+                hasil = alas * tinggi;
+                luas(hasil);
+            }
+            
+            // Keliling
+            else if (rumusJajarGenjang == "2" || rumusJajarGenjang == "keliling") {
+                int sisi1, sisi2;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung keliling jajar genjang" << endl;
+                cout << "Masukkan sisi 1 : "; cin >> sisi1;
+                cout << "Masukkan sisi 2 : "; cin >> sisi2;
+
+                hasil = 2 * (sisi1 * sisi2);
+                keliling(hasil);
+            }
+
+            else {
+                salahInput();
+            }
+        }
+
+        // Belah Ketupat
+        else if (bangunDatar == "5" || bangunDatar == "belaketupat") {
+            menuRumusBelahKetupat();
+            string rumusBelahKetupat;
+            cout << "Pilih : "; cin >> rumusBelahKetupat;
+
+            // Luas
+            if (rumusBelahKetupat == "1" || rumusBelahKetupat == "luas") {
+                int alas, tinggi;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung luas belah ketupat" << endl;
+                cout << "Masukkan alas   : "; cin >> alas;
+                cout << "Masukkan tinggi : "; cin >> tinggi;
+
+                hasil = alas * tinggi;
+                luas(hasil);
+            }
+            
+            // Keliling
+            else if (rumusBelahKetupat == "2" || rumusBelahKetupat == "keliling") {
+                int sisi;
+                float hasil;
+                cout << "=========================================" << endl;
+                cout << "Mwnghitung kelilng belah ketupat" << endl;
+                cout << "Masukkan panjang sisi : "; cin >> sisi;
+
+                hasil = 4 * sisi;
+                keliling(hasil);
+            }
+
+            else {
+                salahInput();
+            }
+        }
+
+        // Trapesium
+        else if (bangunDatar == "6" || bangunDatar == "trapesium") {
+            cout << "Coming soon" << endl;
+        }
+
+        // Lingkarang
+        else if (bangunDatar == "7" || bangunDatar == "lingkaran") {
+            menuRumusLingkaran();
+            string rumusLingkaran;
+            cout << "Pilih : "; cin >> rumusLingkaran;
+
+            // Luas
+            if (rumusLingkaran == "1" || rumusLingkaran == "luas") {
+                float r, hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung luas Lingkaran" << endl;
+                cout << "Masukkan jari-jari : "; cin >> r;
+
+                hasil = PHI * pow(r, 2);
+                luas(hasil);
+            }
+            
+            // Keliling
+            else if (rumusLingkaran == "2" || rumusLingkaran == "keliling") {
+                float r, hasil;
+                cout << "=========================================" << endl;
+                cout << "Menghitung keliling Lingkaran" << endl;
+                cout << "Masukkan jari-jari : "; cin >> r;
+
+                hasil = 2 * PHI * r;
+                keliling(hasil);
+            }
+        }
         else {
             salahInput();
         }
+        cout << "\nKembali kemenu utama [Y/N] : ";  cin >> ulang;
     }
-
-    // Segitiga
-    else if(bangunDatar == 3) {
-        // Segitiga, mau dibuatin cabang segitiga lagi seperti
-        // segitiga sama sisi, siku siku, sembarang, sama kaki dll
-        }
-
-    // Jajar Genjang
-    else if(bangunDatar == 4) {
-        menuRumusJajarGenjang();
-        int rumusJajarGenjang;
-
-        cout << "Pilih [1/2] : ";
-        cin >> rumusJajarGenjang;
-
-        if(rumusJajarGenjang == 1) {
-            double alas, tinggi, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung luas Jajar genjang" << endl;
-            cout << "Masukkan alas : ";
-            cin >> alas;
-            cout << "Masukkan tinggi : ";
-            cin >> tinggi;
-
-            hasil = alas * tinggi;
-            luas(hasil);
-        }
-
-        else if(rumusJajarGenjang == 2) {
-            double sisi1, sisi2, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung keliling jajar genjang" << endl;
-            cout << "Masukkan sisi 1 : ";
-            cin >> sisi1;
-            cout << "Masukkan sisi 2 : ";
-            cin >> sisi2;
-
-            hasil = 2 * (sisi1 * sisi2);
-            keliling(hasil);
-        }
-
-        else {
-            salahInput();
-        }
-    }
-
-    // Belah Ketupat
-    else if(bangunDatar == 5) {
-        menuRumusBelahKetupat();
-        int rumusBelahKetupat;
-
-        cout << "Pilih [1/2] : ";
-        cin >> rumusBelahKetupat;
-
-        if(rumusBelahKetupat == 1) {
-            double alas, tinggi, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung luas belah ketupat" << endl;
-            cout << "Masukkan alas : ";
-            cin >> alas;
-            cout << "Masukkan tinggi : ";
-            cin >> tinggi;
-
-            hasil = alas * tinggi;
-            luas(hasil);
-        }
-
-        else if(rumusBelahKetupat == 2) {
-            double sisi, hasil;
-            cout << "=========================================" << endl;
-            cout << "Mwnghitung kelilng belah ketupat" << endl;
-            cout << "Masukkan panjang sisi : ";
-            cin >> sisi;
-
-            hasil = 4 * sisi;
-            keliling(hasil);
-        }
-
-        else {
-            salahInput();
-        }
-    }
-
-    // Trapesium
-    else if(bangunDatar == 6) {
-        // Coming soon
-    }
-
-    // Lingkarang
-    else if(bangunDatar == 7) {
-        menuRumusLingkaran();
-        int rumusLingkaran;
-
-        cout << "Pilih [1/2] : ";
-        cin >> rumusLingkaran;
-
-        if(rumusLingkaran == 1) {
-            double phi = 3.14, r, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung luas Lingkaran" << endl;
-            cout << "Masukkan jari-jari : ";
-            cin >> r;
-
-            hasil = phi * pow(r, 2);
-            luas(hasil);
-        }
-
-        else if(rumusLingkaran == 2) {
-            double phi = 3.14, r, hasil;
-            cout << "=========================================" << endl;
-            cout << "Menghitung keliling Lingkaran" << endl;
-            cout << "Masukkan jari-jari : ";
-            cin >> r;
-
-            hasil = 2 * phi * r;
-            keliling(hasil);
-        }
-    }
-
-    else {
-        salahInput();
-    }
+    while(ulang == 'Y' || ulang == 'y');
+    cout << endl;
+    cout << "Program akan dimatikan dalam 3 detik"; sleep(3);
+    cout << endl;
+    return 0;
 }
